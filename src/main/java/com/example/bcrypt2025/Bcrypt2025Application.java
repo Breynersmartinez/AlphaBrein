67adfd3eea2d02284dd6ebe591b311923703b720
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-04-12 16:26:01 -0500

    Se cargo el directorio src

---
+++ src/main/java/com/example/bcrypt2025/Bcrypt2025Application.java
@@ -0,0 +1,13 @@
+package com.example.bcrypt2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bcrypt2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bcrypt2025Application.class, args);
+	}
+
+}
